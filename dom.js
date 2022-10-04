920beec414845eb01ff30542e7eb09a3dc82f7bc
aakash-dhamaniya <AakashDhamaniya.dev@gmail.com>
2022-10-04 15:21:55 +0530

    add dom

---
+++ dom.js
@@ -0,0 +1,6 @@
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000'
+var additems=document.getElementById('addItem')
+additems.style.color="green"
+additems.style.fontWeight='bold'
+
