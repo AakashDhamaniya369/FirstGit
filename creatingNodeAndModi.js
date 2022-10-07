24b33a4479c96494914aae6791acb8e179e9b679
aakash-dhamaniya <AakashDhamaniya.dev@gmail.com>
2022-10-07 14:44:00 +0530

    create node and manupulate it

---
+++ creatingNodeAndModi.js
@@ -0,0 +1,24 @@
+//Treversing the dom
+let itemList =document.querySelector('#items')
+//parantNOde
+// console.log(itemList.parentNode)
+// itemList.parentNode.style.backgroundColor="grey"
+// console.log(itemList.parentNode.parentNode.parentNode)
+//parentElement
+// console.log(itemList.parentElement)
+
+//childNodes
+// console.log(itemList.childNodes);
+  // createElement 
+  //crreate a div
+  var newDiv= document.createElement('div');
+  console.log(newDiv)
+  newDiv.className='hello';
+  console.log(newDiv)
+  // crateing text node
+  var newDivText= document.createTextNode('hello');
+  //add text to div
+  newDiv.appendChild(newDivText);
+  var container =document.querySelector('header .container')
+  var h1 =document.querySelector('header  h1');
+    container.insertBefore(newDiv,h1)
