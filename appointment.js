7ab2480aed281316b6c3bec6fa764352e16ca137
aakash-dhamaniya <AakashDhamaniya.dev@gmail.com>
2022-11-07 14:13:14 +0530

    store data in crud instead of localStorage

--- appointment.js
+++ appointment.js
@@ -12,29 +12,49 @@ function store(e)
         name,
         email
     }
-    localStorage.setItem(users.email,JSON.stringify(users))
+    // localStorage.setItem(users.email,JSON.stringify(users))
+    //store data to server
+    axios.post('https://crudcrud.com/api/cf97864e6e0d456c8fe8d15d2e500798/appointment',users
+        ).then(res=>showData(res.data)).catch(err=>console.log(err))
+    
     showData(users);
     
 }
 
 window.addEventListener('DOMContentLoaded',()=>{
-    const localStoragekeys=Object.keys(localStorage)
-    const  localStorageObj=localStorage;
-    for(var i=0;i<localStoragekeys.length;i++)
-    {
-        const key=localStoragekeys[i];
-        const userDetailsString =localStorageObj[key];
-        const userDetailsobj=JSON.parse(userDetailsString);
-        showData(userDetailsobj)
-    }
+    // const localStoragekeys=Object.keys(localStorage)
+    // const  localStorageObj=localStorage;
+    // for(var i=0;i<localStoragekeys.length;i++)
+    // {
+    //     const key=localStoragekeys[i];
+    //     const userDetailsString =localStorageObj[key];
+    //     const userDetailsobj=JSON.parse(userDetailsString);
+    //     showData(userDetailsobj)
+    // }
+    // show data from crud crud
+    let arr=[];
+    const crudObj=axios.get('https://crudcrud.com/api/cf97864e6e0d456c8fe8d15d2e500798/appointment')
+    crudObj.then(res=>{
+        for(let i=0;i<res.data.length;i++)
+        {
+            arr[i]=res.data[i];
+        }
+    })
+    console.log(arr)
+    //  for(var i=0;i<crudKey.length;i++)
+    //     {
+    //     const key=crudKey[i];
+    //     const userDetailsString =crudObj[key];
+    //     const userDetailsobj=JSON.parse(userDetailsString);
+    //     console.log("hello")
+        
+    //  }
 })
 
 function showData(user)
 {
     const parentNode=document.getElementById('listUsers');
-    // if(localStorage.getItem(user.email) !== null){
-    //     removeUserFromScreen(user.email)
-    // }
+    
     const childHTML=`<li id=${user.email}> ${user.name} - ${user.email}
     <button onclick=deleteuser('${user.email}')>X</button>
     <button onclick=editUser('${user.email}','${user.name}')>✎</button>
